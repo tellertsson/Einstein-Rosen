d29fb98d7d89f716d6d1eab07926bbeac08d16af
tressell <tess-e@live.se>
2021-03-10 21:40:35 +0100

    final changes

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 let footnotes = {}
+let expanded = false;
 
 function footnote(n) {
     const footnote = document.getElementById("footnote" + n);
     footnote.style.display = footnotes[n] ? "none" : "block";
     footnotes[n] = footnotes[n] ? false : true;
+}
+
+function expandqr() {
+    const small_element = document.getElementById("qr-code");
+    const large_element = document.getElementById("qr-code-page");
+    if (!expanded) {
+        small_element.className = "expanded";
+        large_element.style.height = "100vh";
+    } else {
+        small_element.className = "";
+        large_element.style.height = "0";
+    }
+    expanded = !expanded;
 }
